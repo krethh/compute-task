da269ad3b0376eac816866a3794d41fec73e6941
krethh <krecik.kret@gmail.com>
2020-10-10 13:21:28 +0200

    Random breaks

--- src/main/java/pl/kulig/Main.java
+++ src/main/java/pl/kulig/Main.java
@@ -1,16 +1,29 @@
 package pl.kulig;
 
+import java.util.Random;
+
 public class Main {
+
+    public static Random random = new Random();
 
     public static void main(String[] args) throws InterruptedException {
         final int iterations = Integer.parseInt(System.getenv("NUMBER_ITERATIONS"));
+        int lambda = 10;
+
+        if (System.getenv().containsKey("LAMBDA")) {
+            lambda = Integer.parseInt(System.getenv("LAMBDA"));
+        }
 
         for (int i = 0; i < iterations; i++) {
             final int computation = i * i;
 
-            Thread.sleep((long) i % 10);
+            Thread.sleep((long) getBreakLength(lambda));
         }
 
         System.out.println("Finished");
+    }
+
+    private static Integer getBreakLength(double lambda) {
+        return (int) Math.round(Math.log(1 - random.nextDouble())/(- lambda));
     }
 }
