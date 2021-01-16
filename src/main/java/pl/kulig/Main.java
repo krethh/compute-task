8fb3ecc5f336e42edb6c01d6be393eda54b58e65
krethh <krecik.kret@gmail.com>
2021-01-16 12:14:42 +0100

    Fix

--- src/main/java/pl/kulig/Main.java
+++ src/main/java/pl/kulig/Main.java
@@ -9,40 +9,20 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
         final int iterations = Integer.parseInt(System.getenv("NUMBER_ITERATIONS"));
         int numThreads = 3;
-        int lambda = 10;
 
-        if (System.getenv().containsKey("LAMBDA")) {
-            lambda = Integer.parseInt(System.getenv("LAMBDA"));
-        }
         if (System.getenv().containsKey("NUMBER_THREADS")) {
             numThreads = Integer.parseInt(System.getenv("NUMBER_THREADS"));
         }
 
         for (int j = 0; j < numThreads; j++) {
-            int finalLambda = lambda;
             new Thread(() -> {
                 for (int i = 0; i < iterations; i++) {
-                    final int computation = i * i;
-
-                    final Integer breakLength = getBreakLength(i, finalLambda);
-                    System.out.println(breakLength);
-                    try {
-                        Thread.sleep(breakLength);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
+                    int computation = i * i;
+                    computation++;
                 }
             }).start();
         }
 
         System.out.println("Finished");
-    }
-
-    private static Integer getBreakLength(int i, int lambda) {
-        if (i % lambda == 0) {
-            return 1;
-        } else {
-            return 0;
-        }
     }
 }
